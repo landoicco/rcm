f380815bd452765e5b651c8a1c0cbb3f947a1399
Lando <landoicco@gmail.com>
2024-11-24 19:13:57 -0600

    Create new pet with address fully working

--- client/src/components/forms/AddPet.jsx
+++ client/src/components/forms/AddPet.jsx
@@ -5,6 +5,7 @@ import { PETS_API_BASE_URL } from "../../commons/endpoints";
 const AddPet = () => {
   const nameRef = useRef();
   const typeRef = useRef();
+  const addressRef = useRef();
 
   const submitPet = (e) => {
     e.preventDefault();
@@ -14,8 +15,11 @@ const AddPet = () => {
       type: typeRef.current.value,
     };
 
+    // Get address from user input
+    const [street, extNumber] = addressRef.current.value.split(" ");
+
     // API POST request
-    fetch(`${PETS_API_BASE_URL}/add/3`, {
+    fetch(`${PETS_API_BASE_URL}/add/${street}/${extNumber}`, {
       method: "POST",
       headers: {
         "content-type": "application/json",
@@ -28,7 +32,7 @@ const AddPet = () => {
 
   return (
     <div className="container">
-      <h3 className="center">Add new pet to </h3>
+      <h3 className="center">Add new pet</h3>
       <form onSubmit={(e) => submitPet(e)}>
         <div className="row">
           <div className="input-field col s10">
@@ -38,6 +42,12 @@ const AddPet = () => {
           <div className="input-field col s2">
             <input ref={typeRef} type="text" className="validate" required />
             <label htmlFor="type">Type</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input ref={addressRef} type="text" className="validate" required />
+            <label htmlFor="name">Address</label>
           </div>
         </div>
         <div className="row center">
